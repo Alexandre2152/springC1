19ace5f7be1558d42464e792d921ec25eee6571e
alexandre251387 <ajfs2513@gmail.com>
2022-03-10 12:03:13 -0300

    Inserte - list

--- src/main/java/br/com/alexandre/VendasApplication.java
+++ src/main/java/br/com/alexandre/VendasApplication.java
@@ -1,12 +1,31 @@
 package br.com.alexandre;
 
+import br.com.alexandre.domain.entity.Cliente;
+import br.com.alexandre.domain.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes){
+        return args -> {
+            clientes.salvar(new Cliente("Alexandre"));
+            clientes.salvar(new Cliente("Santana"));
+            clientes.salvar(new Cliente("ALexandre Santana"));
+
+            List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
